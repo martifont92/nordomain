3e5fe6e0148d538843f45e05f3700bea197a53ac
martifont92 <martifont92@gmail.com>
2020-09-25 17:34:07 +0200

    first commit

---
+++ flaskr/main.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template 
+from flask_login import login_required, current_user
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@main.route('/faqs')
+def faqs():
+    return render_template('faqs.html')
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
